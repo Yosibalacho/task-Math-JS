c78ba22a4ffd6d3f79236818a8313cc22d1ade5e
Yosibalacho <yosibala@gmail.com>
2022-05-25 09:12:04 +0300

    Math

---
+++ main.js
@@ -0,0 +1,82 @@
+// פונקציות - math//
+
+// function randomBetween0To10() {
+//     var rnd= Math.floor(Math.random()*10);
+
+//     console.log(rnd);
+//     return rnd
+// };
+// randomBetween0To10();
+
+// function returnRandomNum(num) {
+
+//     var rnd = Math.floor(Math.random()*num+1) ;
+
+//     return rnd ;
+// }
+// returnRandomNum(34);
+
+// function succsesOrFail(randomNum) {
+//     var rnd=Math.floor(Math.random()*11)
+//     if(rnd==randomNum){
+//        document.write("כל הכבוד")
+//     }
+//   else{
+//     document.write("נכשלת")
+//   }
+// }
+// succsesOrFail(prompt("Enter number between 0-10"));
+
+// function randomFrom0To100() {
+//   var array10 = []
+//   for (var i=0; i < 10; i++) {
+//     array10.push(Math.floor(Math.random()*100));
+//   }
+//   console.log (array10)
+// }
+// randomFrom0To100()
+
+
+
+// function maxNumber() {
+//      var num1= prompt("Enter number")
+//      var rnd = Math.floor(Math.random() * 10);
+//      return Math.max(rnd, num1);
+// }
+// maxNumber();
+
+// function minNumFrom5Nums() {
+//     var nums=[];
+//     for(var i=0;i<5;i++){
+//        nums.push(prompt("Enter number"));
+//     }
+//     return Math.min(...nums);
+// }
+// minNumFrom5Nums();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function returnEvenMinOrOddMax() {
+//     var rnd=Math.floor(Math.random()*100);
+//     var userNum=+prompt("Enter Number");
+//     if(userNum%2==0){
+//            return Math.min(rnd,userNum);
+//        }
+//        else{
+//            return Math.max(rnd,userNum);
+//        };
+// };
+// returnEvenMinOrOddMax();
